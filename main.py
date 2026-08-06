bba1c9befe3d9b48a1f24ab6c704f88b2ea3ff02
KhoaLG <legiakhoa0@gmail.com>
2026-08-06 16:38:25 -0700

    Histogram chart creation based on average match rank

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import urllib
 import json
 import requests
 import re
+from datetime import datetime
+
+# plotting historgram
+from matplotlib import pyplot as plt
+import numpy as np
 """
 3 general statistic page for ranked 
 what are we tracking?
@@ -11,14 +16,16 @@ what are we tracking?
 
 3.1 get 10 most recent ranked matches of the day
 3.2 for each individual match, get the rank of each player -> average that
+3.3 plot a histogram and display the trends of matches
 """
-def get100():
+def get_matches():
+    unix_timestamp = int((datetime.now() - datetime(1970, 1, 2)).total_seconds())
     url = "https://api.deadlock-api.com/v1/matches/metadata"
     params = {
         'include_more_info': 'true', 
         'match_mode': 'ranked',
-        'min_unix_timestamp': 1785826800,
-        'limit': 10
+        'min_unix_timestamp': unix_timestamp,
+        'limit': 100
         }
     
     response = requests.get(url, params=params)
@@ -27,7 +34,7 @@ def get100():
     with open("out.txt", "w") as file:
         file.write(byte_j)
         
-def get_avr_rank():
+def get_avr_rank(rankcount):
     # for each of the match in out.txt get the average for that and then put that data in an array I guess 
     # open file out.txt and get 10 match_id
     ids = []
@@ -59,22 +66,9 @@ def get_avr_rank():
                         searched_phrase = re.search(r"\d+", line)
                         start_index, end_index = searched_phrase.start(), searched_phrase.end()
                         list_extracted_data.append(int(line[start_index:end_index]))
-        calc_rank(list_extracted_data)           
+        calc_rank_and_display(list_extracted_data, rankcount)           
 
-def calc_rank(list_extracted_data):
-    dict = {
-        1 : 'Initiate',
-        2 : 'Seeker',
-        3 : 'Acolyte',
-        4 : 'Sentinel',
-        5 : 'Mystic',
-        6 : 'Ritualist',
-        7 : 'Emissary',
-        8 : 'Oracle',
-        9 : 'Phantom',
-        10 : 'Ascendant',
-        11 : 'Eternus'
-    }
+def calc_rank_and_display(list_extracted_data, rankcount):
     unranked = 12
     total = 0
     for rank in list_extracted_data:
@@ -83,12 +77,20 @@ def calc_rank(list_extracted_data):
         total += rank
         
     final = int((total / unranked) / 10)
-    print(dict.get(final))
-               
-
+    rankcount[final] += 1
+    
+def draw_hist(rankcount):
+    ranks = ['Obscurus', 'Initiate', 'Seeker', 'Acolyte', 'Sentinel', 'Mystic', 'Ritualist', 'Emissary', 'Oracle', 'Phantom', 'Ascendant', 'Eternus']
+    plt.bar(ranks, rankcount)
+    plt.xticks(rotation=45, ha='right', fontsize=12)
+    plt.tight_layout()
+    plt.show()
+    
 def main():    
-    get100()
-    get_avr_rank()
+    rankcount = [0] * 12
+    get_matches()
+    get_avr_rank(rankcount)    
+    draw_hist(rankcount)
     
 if __name__ == '__main__':
     main()
