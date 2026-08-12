48ae612b938e356bdee0ff9bbab0281117a59782
KhoaLG <legiakhoa0@gmail.com>
2026-08-12 12:39:07 -0700

    Each ref displays the match metadata from the match id

--- deadnlock/deadnlock/settings.py
+++ deadnlock/deadnlock/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-*=!@&pfwcm1^-3*#-9*^3-!y*l3pj+^o!rc3qtgox_*i(e=*r=
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,7 +54,7 @@ ROOT_URLCONF = 'deadnlock.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'deadnlock.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -80,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -111,7 +107,6 @@ TIME_ZONE = 'MST'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- deadnlock/ranks/views.py
+++ deadnlock/ranks/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
+import sys
+import json
+import requests
 
 from .models import Match
 
@@ -12,5 +15,19 @@ def index(request):
 
 
 def daily(request, match_id):
-    response = "Here is the result of your match %s."
-    return HttpResponse(response % match_id)
+    template = loader.get_template("ranks/match_details.html")
+    # api call here
+    match_details = {"match_details": get_matches(match_id)}
+    return HttpResponse(template.render(match_details, request))
+
+
+def get_matches(match_id):
+    url = "https://api.deadlock-api.com/v1/matches/metadata"
+    params = {
+        'match_id': match_id,
+    }
+
+    response = requests.get(url, params=params)
+    byte_j = json.dumps(response.json(), separators=(",", ":"), indent=4)
+
+    return byte_j
